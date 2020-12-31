879d0878658ed06bdd412a539b91e5039e499fb5
ax77 <mailbox.r8@gmail.com>
2020-12-31 12:55:32 +0600

    improvement

--- ast_visitors/ApplyExpr.java
+++ ast_visitors/ApplyExpr.java
@@ -34,91 +34,19 @@ public class ApplyExpr {
     ExpressionBase base = e.getBase();
 
     if (base == ExpressionBase.EBINARY) {
-      ExprBinary node = e.getBinary();
-      Token operator = node.getOperator();
-
-      final ExprExpression LHS = node.getLhs();
-      final ExprExpression RHS = node.getRhs();
-
-      applyExpr(object, LHS);
-      applyExpr(object, RHS);
-
-      e.setResultType(node.getLhs().getResultType());
-
+      applyBinary(object, e);
     }
 
     else if (base == ExpressionBase.EPRIMARY_IDENT) {
-      ExprPrimaryIdent primaryIdent = e.getLiteralIdentifier();
-
-      Symbol sym = typeApplier.findBindingFromIdentifierToTypename(primaryIdent.getIdentifier());
-
-      if (sym.isClassType()) {
-        throw new EParseException("unimpl.");
-      }
-
-      // set type to expression
-      final VarDeclarator variable = sym.getVariable();
-      e.setResultType(variable.getType());
-
-      // remember var
-      primaryIdent.setVariable(variable);
-
+      applyIdentifier(e);
     }
 
     else if (base == ExpressionBase.EMETHOD_INVOCATION) {
-      ExprMethodInvocation methodInvocation = e.getMethodInvocation();
-      applyExpr(object, methodInvocation.getObject());
-
-      //TODO:here
-
-      if (methodInvocation.isMethodInvocation()) {
-        // method: a.fn(1,2,3)
-
-        final Type resultTypeOfObject = methodInvocation.getObject().getResultType(); // must be a reference!
-        if (resultTypeOfObject == null || resultTypeOfObject.isPrimitive()) {
-          throw new EParseException("expect reference for method invocation like [a.b()] -> a must be a class.");
-        }
-
-        final ClassDeclaration whereWeWantToFindTheMethod = resultTypeOfObject.getReferenceType().getTypeName();
-        final ClassMethodDeclaration method = whereWeWantToFindTheMethod.getMethod(methodInvocation.getFuncname());
-
-        if (method == null) {
-          throw new EParseException("class has no method: " + methodInvocation.getFuncname().getName());
-        }
-
-        e.setResultType(method.getResultType());
-
-      } else {
-        // function: fn(1,2,3)
-        ClassMethodDeclaration func = object.getMethod(methodInvocation.getFuncname());
-        if (func == null) {
-          throw new EParseException("class has no method: " + methodInvocation.getFuncname().getName());
-        }
-        e.setResultType(func.getResultType());
-      }
-
+      applyMethodInvocation(object, e);
     }
 
     else if (base == ExpressionBase.EFIELD_ACCESS) {
-      ExprFieldAccess fieldAccess = e.getFieldAccess();
-      applyExpr(object, fieldAccess.getObject());
-
-      // find the field, and get its type
-      //
-      final Type resultTypeOfObject = fieldAccess.getObject().getResultType(); // must be a reference!
-      if (resultTypeOfObject == null || resultTypeOfObject.isPrimitive()) {
-        throw new EParseException("expect reference for field access like [a.b] -> a must be a class.");
-      }
-
-      final ClassDeclaration whereWeWantToFindTheField = resultTypeOfObject.getReferenceType().getTypeName();
-      final VarDeclarator field = whereWeWantToFindTheField.getField(fieldAccess.getFieldName());
-
-      if (field == null) {
-        throw new EParseException("class has no field: " + fieldAccess.getFieldName().getName());
-      }
-
-      e.setResultType(field.getType());
-
+      applyFieldAccess(object, e);
     }
 
     else if (base == ExpressionBase.ETHIS) {
@@ -126,8 +54,7 @@ public class ApplyExpr {
     }
 
     else if (base == ExpressionBase.EPRIMARY_NUMBER) {
-      // TODO:
-      e.setResultType(Type.INT_TYPE);
+      e.setResultType(Type.INT_TYPE); // TODO:
     }
 
     else {
@@ -135,6 +62,92 @@ public class ApplyExpr {
     }
 
     return true;
+
+  }
+
+  private void applyBinary(ClassDeclaration object, ExprExpression e) {
+    ExprBinary node = e.getBinary();
+    Token operator = node.getOperator();
+
+    final ExprExpression LHS = node.getLhs();
+    final ExprExpression RHS = node.getRhs();
+
+    applyExpr(object, LHS);
+    applyExpr(object, RHS);
+
+    e.setResultType(node.getLhs().getResultType());
+  }
+
+  private void applyIdentifier(ExprExpression e) {
+    ExprPrimaryIdent primaryIdent = e.getLiteralIdentifier();
+
+    Symbol sym = typeApplier.findBindingFromIdentifierToTypename(primaryIdent.getIdentifier());
+
+    if (sym.isClassType()) {
+      throw new EParseException("unimpl.");
+    }
+
+    // set type to expression
+    final VarDeclarator variable = sym.getVariable();
+    e.setResultType(variable.getType());
+
+    // remember var
+    primaryIdent.setVariable(variable);
+  }
+
+  private void applyFieldAccess(ClassDeclaration object, ExprExpression e) {
+
+    ExprFieldAccess fieldAccess = e.getFieldAccess();
+    applyExpr(object, fieldAccess.getObject());
+
+    // find the field, and get its type
+    //
+    final Type resultTypeOfObject = fieldAccess.getObject().getResultType(); // must be a reference!
+    if (resultTypeOfObject == null || resultTypeOfObject.isPrimitive()) {
+      throw new EParseException("expect reference for field access like [a.b] -> a must be a class.");
+    }
+
+    final ClassDeclaration whereWeWantToFindTheField = resultTypeOfObject.getReferenceType().getTypeName();
+    final VarDeclarator field = whereWeWantToFindTheField.getField(fieldAccess.getFieldName());
+
+    if (field == null) {
+      throw new EParseException("class has no field: " + fieldAccess.getFieldName().getName());
+    }
+
+    e.setResultType(field.getType());
+
+  }
+
+  private void applyMethodInvocation(ClassDeclaration object, ExprExpression e) {
+
+    ExprMethodInvocation methodInvocation = e.getMethodInvocation();
+    applyExpr(object, methodInvocation.getObject());
+
+    if (methodInvocation.isMethodInvocation()) {
+      // method: a.fn(1,2,3)
+
+      final Type resultTypeOfObject = methodInvocation.getObject().getResultType(); // must be a reference!
+      if (resultTypeOfObject == null || resultTypeOfObject.isPrimitive()) {
+        throw new EParseException("expect reference for method invocation like [a.b()] -> a must be a class.");
+      }
+
+      final ClassDeclaration whereWeWantToFindTheMethod = resultTypeOfObject.getReferenceType().getTypeName();
+      final ClassMethodDeclaration method = whereWeWantToFindTheMethod.getMethod(methodInvocation.getFuncname());
+
+      if (method == null) {
+        throw new EParseException("class has no method: " + methodInvocation.getFuncname().getName());
+      }
+
+      e.setResultType(method.getResultType());
+
+    } else {
+      // function: fn(1,2,3)
+      ClassMethodDeclaration func = object.getMethod(methodInvocation.getFuncname());
+      if (func == null) {
+        throw new EParseException("class has no method: " + methodInvocation.getFuncname().getName());
+      }
+      e.setResultType(func.getResultType());
+    }
 
   }
 
