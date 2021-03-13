194286345a3fa56dd29b7fccfd832d0ec66bfae8
ax77 <mailbox.r8@gmail.com>
2021-03-13 15:09:50 +0600

    clean Cstring code

--- src/main/java/_st7_codeout/CCString.java
+++ src/main/java/_st7_codeout/CCString.java
@@ -6,40 +6,39 @@ public class CCString {
 
     //@formatter:off
     StringBuilder sb = new StringBuilder();
-    sb.append("struct string                                         \n");
-    sb.append("{                                                     \n");
-    sb.append("    char *buffer;                                     \n");
-    sb.append("    size_t len, ref_count;                            \n");
-    sb.append("};                                                    \n");
+    sb.append("struct string                                 \n");
+    sb.append("{                                             \n");
+    sb.append("    char *buffer;                             \n");
+    sb.append("    size_t len;                               \n");
+    sb.append("};                                            \n");
     
-    sb.append("void string_init(string __this, char *buf);           \n");
-    sb.append("void string_deinit(string __this);                  \n");
-    sb.append("void string_destroy(string __this);                   \n");
+    sb.append("void string_init(string __this, char *buf);   \n");
+    sb.append("void string_deinit(string __this);            \n");
+    sb.append("void string_destroy(string __this);           \n");
 
-    sb.append("void string_init(string __this, char *buf)            \n");
-    sb.append("{                                                     \n");
-    sb.append("    assert(__this);                                   \n");
-    sb.append("    assert(buf);                                      \n");
-    sb.append("    __this->buffer = hstrdup(buf);                    \n");
-    sb.append("    __this->len = strlen(buf);                        \n");
-    sb.append("    __this->buffer[__this->len] = '\\0';              \n");
-    sb.append("    __this->ref_count = 1;                            \n");
-    sb.append("}                                                     \n");
-    sb.append("char string_get(string __this, size_t index)          \n");
-    sb.append("{                                                     \n");
-    sb.append("    assert(__this);                                   \n");
-    sb.append("    assert(index < __this->len);                      \n");
-    sb.append("    return __this->buffer[index];                     \n");
-    sb.append("}                                                     \n");
-    sb.append("void string_deinit(string __this)                   \n");
-    sb.append("{                                                     \n");
-    sb.append("}                                                     \n");
-    sb.append("void string_destroy(string __this)                    \n");
-    sb.append("{                                                     \n");
-    sb.append("    assert(__this);                                   \n");
-    sb.append("    free(__this->buffer);                             \n");
-    sb.append("    free(__this);                                     \n");
-    sb.append("}                                                     \n");
+    sb.append("void string_init(string __this, char *buf)    \n");
+    sb.append("{                                             \n");
+    sb.append("    assert(__this);                           \n");
+    sb.append("    assert(buf);                              \n");
+    sb.append("    __this->buffer = hstrdup(buf);            \n");
+    sb.append("    __this->len = strlen(buf);                \n");
+    sb.append("    __this->buffer[__this->len] = '\\0';      \n");
+    sb.append("}                                             \n");
+    sb.append("char string_get(string __this, size_t index)  \n");
+    sb.append("{                                             \n");
+    sb.append("    assert(__this);                           \n");
+    sb.append("    assert(index < __this->len);              \n");
+    sb.append("    return __this->buffer[index];             \n");
+    sb.append("}                                             \n");
+    sb.append("void string_deinit(string __this)             \n");
+    sb.append("{                                             \n");
+    sb.append("}                                             \n");
+    sb.append("void string_destroy(string __this)            \n");
+    sb.append("{                                             \n");
+    sb.append("    assert(__this);                           \n");
+    sb.append("    free(__this->buffer);                     \n");
+    sb.append("    free(__this);                             \n");
+    sb.append("}                                             \n");
     //@formatter:on
 
     return sb.toString();
